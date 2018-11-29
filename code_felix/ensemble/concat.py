ed441e83e4be641a1924a0b17936d60faa3d337a
lali2 <lali2@cisco.com>
2018-11-29 12:16:12 +0800

    38915

--- code_felix/ensemble/concat.py
+++ code_felix/ensemble/concat.py
@@ -11,7 +11,7 @@ def read_sub(file):
     if 'predict_lat' in sub:
         sub.reset_index(inplace=True)
         sub = sub[['r_key', 'predict_lat', 'predict_lon']]
-        logger.debug(sub.shape)
+        logger.debug(f'{file}:{sub.shape}')
         return sub
     else:
         logger.debug(f'=======0\n{sub.head()}')
@@ -49,12 +49,13 @@ list = os.listdir(rootdir)
 # pattern = re.compile(r'.*43959.*h5$')
 
 path_list = [
+'./output/ensemble/level1/0.00000_rf_gp0_450_kwmax_depth4num_round100split_num1model_typerfgp0threshold450filenew=3geo=1026.h5',
+    #'./output/ensemble/level1/0.00000_rf_gp0_450_kwmax_depth4num_round100split_num1model_typerfgp0threshold450filenew=2geo=2101.h5',
+'./output/ensemble/level1/0.00000_rf_gp0_300_kwmax_depth4num_round100split_num1model_typerfgp0threshold300filenew=1geo=1801.h5',
 #'./output/ensemble/level1/0.00000_rf_gp0_400_kwmax_depth4num_round100split_num1precision4model_typerfgp0threshold400filenew=3geo=1026.h5',
 #'./output/ensemble/level1/0.00000_rf_gp0_400_kwmax_depth4num_round100split_num1precision5model_typerfgp0threshold400filenew=5geo=148.h5',
 './output/ensemble/level1/0.00000_rf_gp0_200_kwmax_depth4num_round100split_num1model_typerfgp0threshold200filenew=0geo=423.h5',
-'./output/ensemble/level1/0.00000_rf_gp0_400_kwmax_depth4num_round100split_num1model_typerfgp0threshold400filenew=0geo=423.h5',
 
-'./output/ensemble/level1/0.00000_rf_gp0_400_kwmax_depth4num_round100split_num1model_typerfgp0threshold400filenew=1geo=1801.h5',
 './output/ensemble/level1/0.00000_rf_gp0_400_kwmax_depth4num_round100split_num1model_typerfgp0threshold400filenew=2geo=2101.h5',
 './output/ensemble/level1/0.00000_rf_gp0_400_kwmax_depth4num_round100split_num1model_typerfgp0threshold400filenew=3geo=1026.h5',
 './output/ensemble/level1/0.00000_rf_gp0_400_kwmax_depth4num_round100split_num1model_typerfgp0threshold400filenew=4geo=318.h5',
